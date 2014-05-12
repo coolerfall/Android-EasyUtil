35395afbb4a924b62cc5bbb203a654b85636bf89
Vincent Cheung <coolingfall@gmail.com>
2014-05-12 20:05:18 +0800

    add AES and MD5

---
+++ com/droiddev/utils/MD5.java
@@ -0,0 +1,49 @@
+package com.droiddev.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.util.Log;
+
+/**
+ * MD5 util: this util is used to eccrypt string with MD5.
+ *
+ * @author Vincent Cheung
+ * @since  May. 11, 2014
+ */
+public class MD5
+{
+	private static final String TAG = "MD5";
+	
+	public static String Encrypt(String origin) {
+		MessageDigest md5 = null;
+		
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			Log.d(TAG, "[md5 encrypt error]: " +e.getMessage());
+			e.printStackTrace();
+		}
+		
+		char[] charArray = origin.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
+		
+		for (int i = 0; i < charArray.length; i ++) {
+			byteArray[i] = (byte) charArray[i];
+		}
+		
+		byte[] md5Bytes = md5.digest(byteArray);
+		/* transfer the result to hex string */
+		StringBuffer hexValue = new StringBuffer();
+		for (int i = 0; i < md5Bytes.length; i ++) {
+			int val = ((int) md5Bytes[i]) & 0xff;
+			if (val < 16) {
+				hexValue.append("0");
+			}
+			
+			hexValue.append(Integer.toHexString(val));
+		}
+		
+		return hexValue.toString();
+	}
+}
