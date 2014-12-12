8056fa8641246ad43197a2ca312266fd2a9ab993
Vincent Cheung <coolingfall@gmail.com>
2014-12-12 18:37:11 +0800

    update md5 in Crypto

--- src/com/cooler/util/Crypto.java
+++ src/com/cooler/util/Crypto.java
@@ -39,7 +39,7 @@ public class Crypto {
 		 * @param  origin the string to be encrpted
 		 * @return        hash string
 		 */
-		public static String encrypt(String origin) {
+		public static String hash(String origin) {
 			MessageDigest md5 = null;
 
 			try {
@@ -49,27 +49,14 @@ public class Crypto {
 				return NULL;
 			}
 
-			byte[] byteArray = null;
 			try {
-				byteArray = origin.getBytes("UTF-8");
+				md5.update(origin.getBytes("UTF-8"));
+				BigInteger bi = new BigInteger(1, md5.digest());
+				return bi.toString(16);
 			} catch (UnsupportedEncodingException e) {
 				Log.e(TAG, "[md5 encrypt error]: " + e.getMessage());
 				return NULL;
 			}
-
-			byte[] md5Bytes = md5.digest(byteArray);
-			/* transfer the result to hex string */
-			StringBuffer hexValue = new StringBuffer();
-			for (int i = 0; i < md5Bytes.length; i++) {
-				int val = ((int) md5Bytes[i]) & 0xff;
-				if (val < 16) {
-					hexValue.append("0");
-				}
-
-				hexValue.append(Integer.toHexString(val));
-			}
-
-			return hexValue.toString();
 		}
 		
 		/**
