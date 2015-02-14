3cf8cc3362ecda0e6e313935b49dc671bbd78fa6
Vincent Cheung <coolingfall@gmail.com>
2015-02-14 15:50:46 +0800

    bug fix in Prefers

--- app/src/main/java/com/cooler/util/Prefers.java
+++ app/src/main/java/com/cooler/util/Prefers.java
@@ -32,23 +32,23 @@ public class Prefers {
 		}
 		
 		public void save(String key, int value) {
-			sp.edit().putInt(key, value).apply();
+			sp.edit().putInt(key, value).commit();
 		}
 		
 		public void save(String key, long value) {
-			sp.edit().putLong(key, value).apply();
+			sp.edit().putLong(key, value).commit();
 		}
 		
 		public void save(String key, float value) {
-			sp.edit().putFloat(key, value).apply();
+			sp.edit().putFloat(key, value).commit();
 		}
 		
 		public void save(String key, boolean value) {
-			sp.edit().putBoolean(key, value).apply();
+			sp.edit().putBoolean(key, value).commit();
 		}
 		
 		public void save(String key, String value) {
-			sp.edit().putString(key, value).apply();
+			sp.edit().putString(key, value).commit();
 		}
 		
 		public int getInt(String key, int defValue) {
@@ -76,11 +76,11 @@ public class Prefers {
 		}
 		
 		public void remove(String key) {
-			sp.edit().remove(key).apply();
+			sp.edit().remove(key).commit();
 		}
 		
 		public void clear() {
-			sp.edit().clear().apply();
+			sp.edit().clear().commit();
 		}
 	}
 	
