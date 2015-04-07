96a1810ef7f1900eefa7573ff5751b5cad2db6f8
Vincent Cheung <coolingfall@gmail.com>
2015-04-07 11:00:06 +0800

    add new time util

--- app/src/main/java/com/coolerfall/utils/TimeUtils.java
+++ app/src/main/java/com/coolerfall/utils/TimeUtils.java
@@ -11,6 +11,20 @@ import java.text.SimpleDateFormat;
  */
 public class TimeUtils {
 	/**
+	 * Format millisecond according to format.
+	 *
+	 * @param  millis milliseconds
+	 * @param  format date format(such as: yyyy-MM-dd HH:mm:ss)
+	 * @return        formatted date string
+	 */
+	public static String formatMillis(long millis, String format) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+		Date date = new Date(millis);
+
+		return dateFormat.format(date);
+	}
+
+	/**
 	 * Format unix timestamp according to format.
 	 *
 	 * @param  unixTime unix timestamp
@@ -18,19 +32,16 @@ public class TimeUtils {
 	 * @return          formatted date string
 	 */
 	public static String formatUnix(long unixTime, String format) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat(format);
-		Date date = new Date(unixTime * 1000);
-
-		return dateFormat.format(date);
+		return formatMillis(unixTime * 1000, format);
 	}
 
 	/**
 	 * Format current timestamp according to date format.
 	 *
-	 * @param  format date format
+	 * @param  format date format(such as: yyyy-MM-dd HH:mm:ss)
 	 * @return        formatted date string
 	 */
 	public static String formatNow(String format) {
-		return formatUnix(System.currentTimeMillis()/1000, format);
+		return formatMillis(System.currentTimeMillis(), format);
 	}
 }
